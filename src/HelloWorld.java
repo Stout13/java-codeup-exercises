b18fcb7b61826743e1a8116bfa40a7f70048784e
Stout13 <stout.stephen13@gmail.com>
2022-05-16 11:31:13 -0500

    initial commit and HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("World!");
+
+    }
+}
+
+// javac HelloWorld.java
+// java HelloWorld
+//
+// to run HelloWorld.java fromm the console
