1617e78ad70d712aa996a2ccb554e7bae6bda0ae
Stout13 <stout.stephen13@gmail.com>
2022-05-18 21:57:20 -0500

    StringsExercise

---
+++ src/Bob.java
@@ -0,0 +1,34 @@
+public class Bob {
+    //    Create a class named Bob with a main method for the following exercise.
+//
+
+
+
+//    Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+//    Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+//    He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+//    He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+//    He answers 'Whatever.' to anything else.
+//    Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+//
+//
+//}
+}
+
+//const getSeconds = setInterval(function (){
+//        const date = new Date();
+//        console.log(date);
+//        const seconds = date.getSeconds();
+//        console.log(seconds);
+//        return seconds;
+//        }, 1000);
+//        console.log(getSeconds);
+//        let colorPercentage = 6 * getSeconds;
+//        console.log(colorPercentage);
+//        if (getSeconds > 59) {
+//            console.log(getSeconds);
+//            clearInterval(getSeconds);
+//            let colorPercentage = 6 * getSeconds;
+//        console.log(colorPercentage);
+//        }
---
+++ src/StringExercise.java
@@ -0,0 +1,35 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+//    String Basics.
+//
+//    Create a class named StringExercise with a main method.
+//
+
+//    For each of the following output examples: create a String variable that contains the desired output and print it
+//    out to the console, you can do this with only one String variable and one print statement for each output example.
+//
+//
+
+        String pinkFloyd =     "We don't need no education We don't need no thought control";
+        System.out.println(pinkFloyd);
+//
+//    Check "this" out!, "s inside of "s!
+//
+
+        String check = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(check);
+//    In windows, the main drive is usually C:\
+//
+        String windows = "In windows, the main drive is usually C:\\";
+
+        System.out.println(windows);
+
+        String backslashes = "I can do backslashes \\ double backslashes \\\\, and the amazing triple backslash \\\\\\";
+        System.out.println(backslashes);
+
+
+
+    }
+}
