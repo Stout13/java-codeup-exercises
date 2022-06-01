f629b85a418e75c66776288d9f89a0334ccd6d62
Stout13 <stout.stephen13@gmail.com>
2022-05-31 23:02:14 -0500

    added try catch and valueOf to Input class

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,7 @@
 package util;
 
+import com.sun.source.tree.TryTree;
+
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -9,48 +11,63 @@ public class Input {
         double min = Integer.MIN_VALUE;
         double max = Integer.MAX_VALUE;
     }
+
     public Input() {
         Scanner scanner = new Scanner(System.in);
     }
+
     public String getString() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
+
     public boolean yesNo() {
         Scanner scanner = new Scanner(System.in);
         String YN = scanner.nextLine();
         return Objects.equals(YN, "Y") || Objects.equals(YN, "y") || Objects.equals(YN, "yes") || Objects.equals(YN, "Yes");
     }
-    public double getInt(double intMin, double intMax) {
-        Scanner scanner = new Scanner(System.in);
-        double intgr = scanner.nextDouble();
-        if (intgr > intMin || intgr < intMax) {
-            getInt(intMin, intMax);
+
+//    public double getInt(double intMin, double intMax) {
+//        Scanner scanner = new Scanner(System.in);
+//        double intgr = scanner.nextDouble();
+//        if (intgr > intMin || intgr < intMax) {
+//            getInt(intMin, intMax);
+//        }
+//        return intgr;
+//    }
+
+    public int getInt() {
+//        Scanner scanner = new Scanner(System.in);
+        String intgr = getString();
+        try {
+            Double.valueOf(intgr);
+        } catch (Exception e) {
+            System.out.println("This is not an integer");
+            getDouble();
         }
-        return intgr;
+        return Integer.parseInt(intgr);
     }
-    public double getInt() {
-        Scanner scanner = new Scanner(System.in);
-        double intgr = scanner.nextDouble();
-        if (intgr > Integer.MAX_VALUE || intgr < Integer.MIN_VALUE) {
-            System.out.println("That number is not in the range of an integer, it is too big or too small, " +
-                    "use getDouble, or enter an integer");
-            getInt();
-        }
-        return intgr;
-    }
-    public long getDouble(long doubleMin, long doubleMax) {
-        Scanner scanner = new Scanner(System.in);
-        long intgr = scanner.nextLong();
-        if (intgr > doubleMax || intgr < doubleMin) {
-            System.out.println("That number is not in the range of an integer, it is too big or too small, " +
-                    "use getDouble, or enter an integer");
-            getDouble(doubleMin, doubleMax);
-        }
-        return intgr;
-    }
+
+//    public long getDouble(long doubleMin, long doubleMax) {
+//        Scanner scanner = new Scanner(System.in);
+//        long intgr = scanner.nextLong();
+//        if (intgr > doubleMax || intgr < doubleMin) {
+//            System.out.println("That number is not in the range of an integer, it is too big or too small, " +
+//                    "use getDouble, or enter an integer");
+//            getDouble(doubleMin, doubleMax);
+//        }
+//        return intgr;
+//    }
+
     public double getDouble() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextDouble();
+//        Scanner scanner = new Scanner(System.in);
+        String dbl = getString();
+        try {
+            Double.valueOf(dbl);
+        } catch (Exception e) {
+            System.out.println("This is not a double");
+            getDouble();
+        }
+        return Double.parseDouble(dbl);
     }
 }
